0d771eb789012cd6f7e946b6473c29b288516b2d
pedro17amador <pedro.r.amador98@gmail.com>
2019-04-09 15:03:11 -0300

    Aumenta e diminue

---
+++ js/funcoes.js
@@ -0,0 +1,13 @@
+function trocaFonteMaior() {
+	document.getElementById("demo").style.fontSize="35px";
+}
+
+function trocaFonteMenor() {
+	document.getElementById("demo").style.fontSize="16px";
+}
+function apagarElemento(){
+	document.getElementById("demo").style.display="none";
+}
+function naoApagarElemento(){
+	document.getElementById("demo").style.display="block";
+}
